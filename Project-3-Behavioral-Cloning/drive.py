aac683606c43862a7195788127dfe54481941721
Michele Cavaioni <michele.cavaioni@gmail.com>
2016-12-18 14:42:58 -0500

    feat: updated speed

--- Project-3-Behavioral-Cloning/drive.py
+++ Project-3-Behavioral-Cloning/drive.py
@@ -33,12 +33,14 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
+################
 
     resized = scipy.misc.imresize(image, (20,64))  #new line
     image_array = np.asarray(resized)  #new line
 
     # image_array = np.asarray(image)
     transformed_image_array = image_array[None, :, :, :]
+############################    
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
