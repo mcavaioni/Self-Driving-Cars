aa757c6e923c04aee60ee6ae2631e07f87719435
Michele Cavaioni <michele.cavaioni@gmail.com>
2017-01-05 14:16:31 -0500

    feat: updated threshold and warp values

--- Project-4-Advanced-Lane-Finding/thresholded.py
+++ Project-4-Advanced-Lane-Finding/thresholded.py
@@ -7,7 +7,7 @@ import glob
 
 import calibration
 
-from scipy.signal import find_peaks_cwt
+# from scipy.signal import find_peaks_cwt
 
 path ='/Users/michelecavaioni/Flatiron/My-Projects/Udacity (Self Driving Car)/Project #4 (Advanced Lane Finding)/CarND-Advanced-Lane-Lines/test_images'
 images = glob.glob(path + '/test*.jpg')
@@ -61,7 +61,7 @@ def thres_img(img):
   # plt.show()
 
   #create binary threshold to select pixels based on gradient strength (for color channel S): 
-  s_thresh_min = 175
+  s_thresh_min = 180
   s_thresh_max = 255
   s_binary = np.zeros_like(s_channel)
   s_binary[(s_channel>=s_thresh_min) & (s_channel<=s_thresh_max)]=1
@@ -73,27 +73,48 @@ def thres_img(img):
   # plt.imshow(color_binary)
   # plt.show()
 
+  #luv:
+  luv = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+  l_channel = luv[:,:,0]
+  l_thresh_min = 220
+  l_thresh_max = 255
+  l_binary = np.zeros_like(l_channel)
+  l_binary[(l_channel>=l_thresh_min) & (l_channel<=l_thresh_max)]=1
+  # plt.imshow(l_binary, cmap = 'gray')
+  # plt.show()
+
+  #lab:
+  lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+  lab_channel = luv[:,:,2]
+  lab_thresh_min = 180
+  lab_thresh_max = 255
+  lab_binary = np.zeros_like(lab_channel)
+  lab_binary[(lab_channel>=lab_thresh_min) & (lab_channel<=lab_thresh_max)]=1
+  # plt.imshow(lab_binary, cmap = 'gray')
+  # plt.show()
+
   #combine the two binary thresholds
-  combined_binary = np.zeros_like(sxbinary)
-  combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+  combined_binary = np.zeros_like(l_binary)
+  combined_binary[(lab_binary == 1) | (l_binary == 1) ] = 1
   return combined_binary
   # plt.imshow(combined_binary, cmap='gray')
   # plt.show()
+
+
+
 
 def warp(img):
   '''
   selects areas and applies Perspective Transform to get "bird-eye-view"
   '''
-  # img = mpimg.imread(img)
-  # img = cv2.undistort(img, mtx, dist, None, mtx)
-  # plt.imshow(img)
+
   img_size = (img.shape[1], img.shape[0])
 
   #trapezoidal
-  # plt.plot(200,660, '.')
-  # plt.plot(520,480, '.')
-  # plt.plot(770,480, '.')
-  # plt.plot(1150,660, '.')
+  # plt.plot(180,700, '.')
+  # plt.plot(580,450, '.')
+  # plt.plot(750,450, '.')
+  # plt.plot(1180,700, '.')
 
   #rectangle
   # plt.plot(0,img.shape[0], '.')
@@ -101,34 +122,28 @@ def warp(img):
   # plt.plot(img.shape[1],0, '.')
   # plt.plot(img.shape[1],img.shape[0], '.')
 
-  src = np.float32([[200,660],
-                    [520,480],
-                    [770,480],
-                    [1150,660]])
+  src = np.float32([[240,720],
+                    [575,460],
+                    [715,460],
+                    [1150,720]])
 
-  dst = np.float32([[0,img.shape[0]],
-                    [0,0],
-                    [img.shape[1],0],
-                    [img.shape[1],img.shape[0]]])
+  dst = np.float32([[240,720],
+                    [240,0],
+                    [1150,0],
+                    [1150,720]])
 
   M = cv2.getPerspectiveTransform(src,dst)
-  warped = cv2.warpPerspective(img, M, img_size, flags = cv2.INTER_LINEAR)
+  warped = cv2.warpPerspective(img, M, img_size)#, flags = cv2.INTER_LINEAR)
   return warped
 
-# for img in images:
-img = images[5]
-img = mpimg.imread(img)
-#apply distortion correction to the raw image
-img = cv2.undistort(img, mtx, dist, None, mtx)
-thresholded = thres_img(img)
-warped_img = warp(thresholded)
-# plt.imshow(warped_img, cmap='gray')
-histogram = np.sum(warped_img[warped_img.shape[0]/2:,:], axis=0)
-# plt.plot(histogram)
-# plt.show()
 
-#finding right and left lanes pixels:
+
+
 def lines_pixels(img):
+  '''
+  finds right and left lane lines
+  '''
+
   right_lane_x = []
   right_lane_y = []
   left_lane_x = []
@@ -140,16 +155,16 @@ def lines_pixels(img):
 
   x_val = init_peak_left
   previous = 0
-  for i in reversed(range(10)):
-    img_y1 = img.shape[0]-img.shape[0]*i/10
-    img_y2 = img.shape[0]-img.shape[0]*(i+1)/10
+  for i in (range(100)):
+    img_y1 = img.shape[0]-img.shape[0]*i/100
+    img_y2 = img.shape[0]-img.shape[0]*(i+1)/100
 
     histogram = np.sum(img[img_y2:img_y1,:img.shape[1]/2], axis=0)
     x_val = int(np.argmax(histogram))
-    y_val = int(720-i*img.shape[0]/10)
+    y_val = int(720-i*img.shape[0]/100)
     if (y_val == 0 or x_val == 0):
       pass
-    elif (abs(x_val-previous) > 100 and not(i == 9) and not(previous == 0)):
+    elif (abs(x_val-previous) > 100 and not(i == 99) and not(previous == 0)):
       pass
     else:
       left_lane_x.append(x_val)
@@ -162,23 +177,23 @@ def lines_pixels(img):
 
   x_val = init_peak_right
   previous = 0
-  for i in reversed(range(10)):
-    img_y1 = img.shape[0]-img.shape[0]*i/10
-    img_y2 = img.shape[0]-img.shape[0]*(i+1)/10
+  for i in (range(100)):
+    img_y1 = img.shape[0]-img.shape[0]*i/100
+    img_y2 = img.shape[0]-img.shape[0]*(i+1)/100
 
     histogram = np.sum(img[img_y2:img_y1,(img.shape[1]/2):], axis=0)
     x_val = int(np.argmax(histogram)) + 640
-    y_val = int(720-i*img.shape[0]/10)
+    y_val = int(720-i*img.shape[0]/100)
     if (y_val == 0 or x_val == 640):
       pass
-    elif (abs(x_val-previous) > 100 and not(i == 9) and not(previous == 0)):
+    elif (abs(x_val-previous) > 100 and not(i == 99) and not(previous == 0)):
       pass
     else:
       right_lane_x.append(x_val)
       right_lane_y.append(y_val)
       previous = x_val
           
-
+  #plot the pixels:
   plt.imshow(img)
   plt.plot(left_lane_x, left_lane_y, 'o', color='yellow')
   plt.plot(right_lane_x, right_lane_y, 'o', color = 'blue')
@@ -197,6 +212,54 @@ def lines_pixels(img):
   plt.plot(right_fitx, right_lane_y, color='green', linewidth=3)
   plt.show()
 
-lines_pixels(warped_img)
-# plt.show()
+  return left_lane_y, left_fitx, right_fitx, left_fit, right_fit
+
+def radius(left_lane_y, left_fit, right_fit):
+  y_eval = np.max(left_lane_y)
+  left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) \
+                               /np.absolute(2*left_fit[0])
+  right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) \
+                                  /np.absolute(2*right_fit[0])
+  print(left_curverad, right_curverad)  
+
+# def mask(img):
+#   '''
+#   masks the area around the lane lines, making black the rest
+#   '''
+#   mask = np.zeros_like(img)   
+
+#   if len(img.shape) > 2:
+#       channel_count = img.shape[2]  
+#       ignore_mask_color = (255,) * channel_count
+#   else:
+#       ignore_mask_color = 255
+      
+#   vertices = np.array([[(250,700),(550,450),(750,450),(1200,700)]])
+#   #filling pixels inside the polygon defined by "vertices" with the fill color    
+#   cv2.fillPoly(mask, vertices, ignore_mask_color)
+  
+#   #returning the image only where mask pixels are nonzero
+#   masked_image = cv2.bitwise_and(img, mask)
+#   return masked_image
+
+
+for i in images:
+  # image = i
+  image = i
+  image = mpimg.imread(image)
+  #apply distortion correction to the raw image
+  img = cv2.undistort(image, mtx, dist, None, mtx)
+  # thresholded = thres_img(img)
+  # masked = mask(thresholded)
+  # plt.imshow(img)
+  warped_img = warp(img)
+  thresholded = thres_img(warped_img)
+  # plt.show()
+  # plt.imshow(warped_img, cmap='gray')
+  # histogram = np.sum(warped_img[warped_img.shape[0]/2:,:], axis=0)
+  # plt.plot(histogram)
+  # plt.imshow(warped_img)
+  left_lane_y, left_fitx, right_fitx, left_fit, right_fit = lines_pixels(thresholded)
+  radius(left_lane_y, left_fit, right_fit)
+  plt.show()
 
