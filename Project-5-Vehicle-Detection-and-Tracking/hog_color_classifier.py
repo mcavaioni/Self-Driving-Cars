ea0f8cc0bf602388e7ab99a4b630b8e04cb452d3
Michele Cavaioni <michele.cavaioni@gmail.com>
2017-01-16 09:56:46 -0500

    feat: created feature vector and classifier

---
+++ Project-5-Vehicle-Detection-and-Tracking/hog_color_classifier.py
@@ -0,0 +1,158 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import cv2
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+# from sklearn.cross_validation import train_test_split
+
+#.astype('uint8'))
+
+#import from pickle non-vehicle images
+with open('non_vehicle.pickle', 'rb') as handle:
+    non_vehicle = pickle.load(handle)
+
+#import from pickle car images
+with open('car.pickle', 'rb') as handle:
+    car = pickle.load(handle)
+
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+  '''
+  computes histogram of color features 
+  '''
+  # Compute the histogram of the color channels separately
+  channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+  channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+  channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+  # Concatenate the histograms into a single feature vector
+  hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+  # Return the individual histograms, bin_centers and feature vector
+  return hist_features
+
+
+def bin_spatial(img, size=(32, 32)):
+  '''
+  computes binned color features 
+  '''
+  # Use cv2.resize().ravel() to create the feature vector
+  features = cv2.resize(img, size).ravel() 
+  # Return the feature vector
+  return features
+
+#NOTE: input image is grayscale
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
+                        vis=False, feature_vec=True):
+    '''
+    returns HOG features and visualization. Input is a grascale image.
+    '''
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                       visualise=vis, feature_vector=feature_vec)
+        return features
+
+# # Plot HOG:
+# gray = cv2.cvtColor((car[0]), cv2.COLOR_RGB2GRAY)
+# features, hog_image = get_hog_features(gray, orient=9, pix_per_cell=8, cell_per_block=2,vis=True,feature_vec=False)
+# plt.imshow(hog_image, cmap='gray')
+# plt.show()
+
+def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256), orient=9, 
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0):
+    '''
+    Combines features from a list of images. 
+    Features include: binned colors, histogram of colors, histogram of oriented gradient (HOG)
+    '''
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for file in imgs:
+        # Read in each one by one
+        image=file
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        else: feature_image = np.copy(image)      
+        # Apply bin_spatial() to get spatial color features
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        # Apply color_hist() also with a color space option now
+        hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
+        # Call get_hog_features() with vis=False, feature_vec=True
+        hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
+                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # Append the new feature vector to the features list
+        features.append(np.concatenate((spatial_features, hist_features, hog_features)))
+    # Return list of feature vectors
+    return features
+
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+
+#Create list of feature vectors for cars:
+car_features = extract_features(car, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256), orient=orient, 
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, hog_channel=0)
+#Create list of feature vectors for non-cars:
+notcar_features = extract_features(non_vehicle, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256), orient=orient, 
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, hog_channel=0)
+
+
+#Normalize features:
+# Create an array stack of feature vectors
+X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+
+#create label vector (y=1 for cars and y=0 for non-cars):
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+#Create a classifier and train it on the training set:
+# Use a linear SVC 
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(t2-t, 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Train Accuracy of SVC = ', svc.score(X_train, y_train))
+
+#Find out test accuracy on the test set and predict a single example:
+print('Test Accuracy of SVC = ', svc.score(X_test, y_test))
+# Check the prediction time for a single sample
+t=time.time()
+prediction = svc.predict(X_test[0].reshape(1, -1))
+t2 = time.time()
+print(t2-t, 'Seconds to predict with SVC')
