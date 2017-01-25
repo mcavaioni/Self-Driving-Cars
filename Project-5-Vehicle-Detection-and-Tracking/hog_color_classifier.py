2e47a0be20138271918c25c92ab8c4c286096975
Michele Cavaioni <michele.cavaioni@gmail.com>
2017-01-25 13:27:56 -0500

    feat: added comments to final classifier file

--- Project-5-Vehicle-Detection-and-Tracking/hog_color_classifier.py
+++ Project-5-Vehicle-Detection-and-Tracking/hog_color_classifier.py
@@ -12,17 +12,14 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from skimage.feature import hog
 from sklearn.model_selection import train_test_split
-# from skimage.color import rgb2ycbcr
 # from sklearn.cross_validation import train_test_split
 
-#.astype('uint8'))
-
 #import from pickle non-vehicle images
-with open('non_vehicle4.pickle', 'rb') as handle:
+with open('non_vehicle5.pickle', 'rb') as handle:
     non_vehicles = pickle.load(handle)
 
 #import from pickle car images
-with open('car4.pickle', 'rb') as handle:
+with open('car5.pickle', 'rb') as handle:
     cars = pickle.load(handle)
 
 
@@ -49,7 +46,7 @@ def bin_spatial(img, size=(32, 32)):
   # Return the feature vector
   return features
 
-#NOTE: input image is grayscale
+
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
     '''
@@ -68,17 +65,12 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                        visualise=vis, feature_vector=feature_vec)
         return features
 
-# # Plot HOG:
-# gray = cv2.cvtColor((cars[0]), cv2.COLOR_RGB2GRAY)
-# features, hog_image = get_hog_features(gray, orient=9, pix_per_cell=8, cell_per_block=2,vis=True,feature_vec=False)
-# plt.imshow(hog_image, cmap='gray')
-# plt.show()
 
 def extract_features(imgs, cspace='YCrCb', spatial_size=(32, 32),
                         hist_bins=32, hist_range=(0, 256), orient=9, 
                         pix_per_cell=8, cell_per_block=2):
     '''
-    Combines features from a list of images. 
+    Combines features from a list of images, after transforming the image in a different space (here transformed from RGB to YCrCb)
     Features include: binned colors, histogram of colors, histogram of oriented gradient (HOG)
     '''
     # Create a list to append feature vectors to
@@ -105,8 +97,6 @@ def extract_features(imgs, cspace='YCrCb', spatial_size=(32, 32),
         # Apply color_hist() also with a color space option now
         hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
         # Call get_hog_features() with vis=False, feature_vec=True
-        # hog_features = get_hog_features((feature_image[:,:,1], orient, 
-        #                 pix_per_cell, cell_per_block, vis=False, feature_vec=True)
         hog_features = []
         for channel in range(feature_image.shape[2]):
             hog_features.append(get_hog_features(feature_image[:,:,channel], 
@@ -115,7 +105,6 @@ def extract_features(imgs, cspace='YCrCb', spatial_size=(32, 32),
         hog_features = np.ravel(hog_features) 
         # Append the new feature vector to the features list
         features.append(np.concatenate((spatial_features, hist_features, hog_features)))
-        # features = hog_features
     # Return list of feature vectors
     return features
 
@@ -126,11 +115,11 @@ cell_per_block = 2
 #Create list of feature vectors for cars:
 car_features = extract_features(cars, cspace='YCrCb', spatial_size=(32, 32),
                         hist_bins=32, hist_range=(0, 256), orient=orient, 
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block)#, hog_channel=0)
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block)
 #Create list of feature vectors for non-cars:
 notcar_features = extract_features(non_vehicles, cspace='YCrCb', spatial_size=(32, 32),
                         hist_bins=32, hist_range=(0, 256), orient=orient, 
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block)#, hog_channel=0)
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block)
 
 
 #Normalize features:
@@ -151,13 +140,11 @@ rand_state = np.random.randint(0, 100)
 X_train, X_test, y_train, y_test = train_test_split(
     scaled_X, y, test_size=0.2, random_state=rand_state)
 
+
 ############
-#Uncomment this out to train the model
 #Create a classifier and train it on the training set:
-# Use a linear SVC 
-#****Prefer dual=False when n_samples > n_features***
 clf = LinearSVC(penalty='l1',dual=False, C=100) 
-# clf = SVC()
+
 
 # Check the training time for the SVC
 t=time.time()
@@ -177,123 +164,6 @@ print(t2-t, 'Seconds to predict with SVC')
 
 #Save the trained model
 from sklearn.externals import joblib
-joblib.dump(clf, 'saved_model4.pkl') 
-#saving the scaler:
-joblib.dump(X_scaler, 'saved_scaler4.pkl') 
-
-
-#############
-
-#Reload saved model:
-from sklearn.externals import joblib
-saved_clf = joblib.load('saved_model4.pkl') 
-saved_scaler = joblib.load('saved_scaler4.pkl')
-#make new prediction:
-# new_prediction = saved_clf.predict(X_test[100].reshape(1, -1))
-# print("New Prediction: ", new_prediction)
-
-#######################
-#Sliding window:
-
-image = mpimg.imread('/Users/michelecavaioni/Flatiron/My-Projects/Udacity (Self Driving Car)/Project #5 (Vehicle Detection and Tracking/test5.jpg')
-
-
-def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
-    '''
-    draws rectangular boxes based on given vertices
-    '''
-    # Make a copy of the image
-    imcopy = np.copy(img)
-    # Iterate through the bounding boxes
-    for bbox in bboxes:
-        # Draw a rectangle given bbox coordinates
-        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
-    # Return the image copy with boxes drawn
-    return imcopy
-
-
-def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], 
-                    xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
-    '''
-    creates sliding windows throughout the whole image. It outputs a list of vertices to be used to create rectangular boxes
-    '''
-    # If x and/or y start/stop positions not defined, set to image size
-    if x_start_stop==[None, None]:
-      x_start_stop = [0,img.shape[1]]
-    if y_start_stop==[None, None]:
-      y_start_stop = [0,img.shape[0]]
-    # Compute the span of the region to be searched   
-    
-    # Compute the number of pixels per step in x/y
-    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
-    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
-    # Compute the number of windows in x/y
-    num_wind_x = np.int((x_start_stop[1] - x_start_stop[0])/nx_pix_per_step)-1
-    num_wind_y = np.int((y_start_stop[1] - y_start_stop[0])/ny_pix_per_step)-1
-    # num_wind_x = int(x_start_stop[1]/(xy_window[0]*xy_overlap[0]))-1
-    # num_wind_y = int(y_start_stop[1]/(xy_window[1]*xy_overlap[1]))-1
-    
-    
-    # Initialize a list to append window positions to
-    window_list = []
-    # Loop through finding x and y window positions
-    #     Note: you could vectorize this step, but in practice
-    #     you'll be considering windows one by one with your
-    #     classifier, so looping makes sense
-        # Calculate each window position
-        # Append window position to list
-    for iter_x in range(num_wind_x):
-        for iter_y in range(num_wind_y):
-            beg_x = nx_pix_per_step*iter_x + x_start_stop[0]
-            beg_y = ny_pix_per_step*iter_y + y_start_stop[0]
-            end_x = beg_x + xy_window[0]
-            end_y = beg_y + xy_window[1]
-            points = ((beg_x,beg_y),(end_x,end_y))
-            window_list.append(points)
-    # Return the list of windows
-    return window_list
-
-windows = slide_window(image, x_start_stop=[None, None], y_start_stop=[None, None], 
-                    xy_window=(132, 132), xy_overlap=(0.8, 0.8))
-
-
-def check_single_wind(image):
-  detected_boxes = []
-  for one_wind in windows:
-    # one_wind = [one_wind]
-    y1 = one_wind[0][1]
-    y2 = one_wind[1][1]
-    x1 = one_wind[0][0]
-    x2 = one_wind[1][0]
-    crop_img = image[y1:y2,x1:x2]
-    resized = cv2.resize(crop_img,(64,64))
-    res_feat = extract_features([resized], cspace='RGB', spatial_size=(32, 32),
-                        hist_bins=32, hist_range=(0, 256), orient=9, 
-                        pix_per_cell=4, cell_per_block=2, hog_channel=0)
-    res_feat_norm = saved_scaler.transform(res_feat)
-    new_prediction = saved_clf.predict(res_feat_norm)
-    if (new_prediction[0]) ==1.0:
-      detected_boxes.append(one_wind)
-  window_img = draw_boxes(image, detected_boxes, color=(0, 0, 255), thick=6)
-  plt.imshow(window_img)
-  plt.show()
-
-check_single_wind(image)
-# plt.show()
-
-###########
-# one_wind = [(windows[1])]
-#crop image
-# crop_img = img[y: y + h, x: x + w]
-# y1 = one_wind[0][0][1]
-# y2 = one_wind[0][1][1]
-# x1 = one_wind[0][0][0]
-# x2 = one_wind[0][1][0]
-# crop_img = image[y1:y2,x1:x2]
-#  RESIZE TO 64x64 BEFORE FEEDING CLASSIFIER
-
-# #draw the boxes:                       
-# window_img = draw_boxes(image, one_wind, color=(0, 0, 255), thick=6)                    
-# plt.imshow(window_img)
-# # plt.imshow(crop_img)
-# plt.show()
+joblib.dump(clf, 'saved_model5.pkl') 
+#Save the scaler:
+joblib.dump(X_scaler, 'saved_scaler5.pkl') 
